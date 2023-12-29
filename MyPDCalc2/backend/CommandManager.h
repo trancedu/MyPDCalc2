74097b63d519695146f05c5619dc6b3d6d02344a
trancedu <92005446+trancedu@users.noreply.github.com>
2023-12-29 11:42:55 -0600

    Implement CommandManager

--- MyPDCalc2/backend/CommandManager.h
+++ MyPDCalc2/backend/CommandManager.h
@@ -1,1 +1,34 @@
 #pragma once
+#include <algorithm>
+#include "CommandFactory.h"
+#include "UserInterface.h"
+
+class CommandManager
+{
+public:
+	CommandManager(UserInterface& ui) : ui_{ ui }, commands(100, nullptr) {}
+	void execute(unique_ptr<Command> command) {
+		command->execute();
+		commands[position_] = std::move(command);
+		position_++;
+	}
+	void undo() {
+		position_--;
+		if (position_ >= 0) {
+			commands[position_]->undo();
+		}
+		else {
+			position_ = 0;
+		}
+	}
+	void redo() {
+		if (commands[position_]) {
+			commands[position_]->execute();
+			std::fill(commands.begin() + position_ + 1, commands.end(), nullptr);
+		}
+	}
+private:
+	UserInterface& ui_;
+	std::vector<unique_ptr<Command>> commands;
+	int position_ = 0;
+};
