1a8f187282e6f6bace387225437f000efa3c273b
trance <trancedu1@gmail.com>
2024-12-29 22:42:14 -0600

    Export stackplugininterface

--- backend/StackPluginInterface.h
+++ backend/StackPluginInterface.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "stack.h"
+#include "export.h"
 
-extern "C" void StackPush(double d, bool notify);
+extern "C" BACKEND_API void StackPush(double d, bool notify);
 
-extern "C" double StackPop(bool notify);
+extern "C" BACKEND_API double StackPop(bool notify);
 
-extern "C" size_t StackSize();
+extern "C" BACKEND_API size_t StackSize();
 
-extern "C" double StackFirstElement();
+extern "C" BACKEND_API double StackFirstElement();
 
-extern "C" double StackSecondElement();
+extern "C" BACKEND_API double StackSecondElement();
 
